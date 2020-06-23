8d7c84dde4f46ab254bb5d18b8a7ad7184e0ff96
NagarajuG <59116728+NagarajuG@users.noreply.github.com>
2020-06-23 15:50:34 +0530

    first commit

---
+++ MvcApplicationAcceptenceTest.Tests/Controllers/ValuesControllerTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcApplicationTest;
+using MvcApplicationTest.Controllers;
+
+namespace MvcApplicationAcceptenceTest.Tests.Controllers
+{
+    [TestClass]
+    public class ValuesControllerTest
+    {
+        [TestMethod]
+        public void AcceptanceTest1()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            IEnumerable<string> result = controller.Get();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("value1", result.ElementAt(0));
+            Assert.AreEqual("value2", result.ElementAt(1));
+        }
+
+        [TestMethod]
+        public void AcceptanceTest2()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            string result = controller.Get(5);
+
+            // Assert
+            Assert.AreEqual("value", result);
+        }
+
+        [TestMethod]
+        public void AcceptanceTest3()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            controller.Post("value");
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void AcceptanceTest4()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            controller.Put(5, "value");
+
+            // Assert
+        }
+                
+        [TestMethod]
+        public void AcceptanceTest5()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            controller.Delete(5);
+
+            // Assert
+        }
+    }
+}
